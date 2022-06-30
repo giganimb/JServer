5944a34ba6e86f5e407c62ffc386786fe1eb802a
Ilya Tovpeko <mcilyasat@mail.ru>
2022-06-30 13:24:22 +0300

    added .env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+require("dotenv").config();
 
 const postRoutes = require("./routes/post-routes");
 const postApiRoutes = require("./routes/api-post-routes");
@@ -8,21 +9,19 @@ const contactRoutes = require("./routes/contact-routes");
 const contactApiRoutes = require("./routes/api-contact-routes");
 const createPath = require("./helpers/create-path");
 
+
 const app = express();
 
 app.set("view engine", "ejs");
 
-const PORT = 3000;
-const db = "mongodb+srv://qwerty:1234567890@cluster0.52o1j.mongodb.net/node-course?retryWrites=true&w=majority";
-
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to DB"))
     .catch((error) => console.log(error));
 
 
-app.listen(PORT, (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}...`);
+app.listen(process.env.PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${process.env.PORT}...`);
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -33,7 +32,6 @@ app.use(methodOverride("_method"));
 
 
 app.get("/", (req, res) => {
-    // res.send("<h1>Hello world!</h1>");
     const title = 'Home';
     res.render(createPath("index"), { title });
 });
